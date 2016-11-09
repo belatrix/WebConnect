0af40d26b05b4d77837b733eb7426fef6c43849a
maxi <rpenaranda@belatrixsf.com>
2016-11-09 16:11:35 -0300

    Added routing to tabs programmaticaly

--- src/app/home/tab-group/tab-group.component.ts
+++ src/app/home/tab-group/tab-group.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab-group',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab-group.component.css']
 })
 export class TabGroupComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  public changeTab(e) {
+    switch (e.index) {
+      case 0:
+        this.router.navigateByUrl('/account');
+        break;
+      case 1:
+        this.router.navigateByUrl('/ranking');
+        break;
+      case 2:
+        this.router.navigateByUrl('/activity');
+        break;
+      case 3:
+        this.router.navigateByUrl('/top-tags');
+        break;
+
+    default:
+        // TODO, navigate to 404 page
+        break;
+    }
+  }
 }
